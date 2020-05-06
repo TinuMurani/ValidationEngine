362bd47fb1d7b3e01d55d3455660a4467df501c7
leontin.murani <leontin.murani@gmail.com>
2020-05-06 17:00:30 +0300

    Minor input change

--- ValidationEngine.ConsoleApp/Program.cs
+++ ValidationEngine.ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ValidationEngine.ConsoleApp
         {
             List<Person> people = new List<Person>()
             {
-                new Person { Nume = "Murani", Prenume = "Tinu", CNP = "1840803055084", Varsta = 35 },
+                new Person { Nume = "Murani", Prenume = "Tinu", CNP = "1840803055213", Varsta = 35 },
                 new Person { Nume = "Banner", Prenume = "Bruce", CNP = "1741201055162", Varsta = 45 },
                 new Person { Nume = "Clark", Prenume = "Kent", CNP = "1811108055358", Varsta = 38 },
                 new Person { Nume = "Parker", Prenume = "Peter", CNP = "5030108055218", Varsta = 17 },
